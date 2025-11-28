ebba0f089847e541a11c1f04f7b5c08137aad815
chengyf <chengyf@dudongx.com>
2025-11-28 15:13:01 +0800

    feat: fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const base = process.env.VERCEL ? '/' : '/christmas-tree/'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/christmas-tree/',
+  base,
 })
